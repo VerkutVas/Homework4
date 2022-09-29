f56d0004741b58147956fb7089d8ee4e72bc21e3
Vasiya <nva163@mail.ru>
2022-09-29 15:30:11 +0400

    Решение задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SummNumber(int num)
+{
+    int counter = num;
+    int advence = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advence = num - num % 10;
+        result = result + (num - advence);
+        num = num / 10;
+    }
+    return result;
+}
+int SumNum = SummNumber(num);
+System.Console.WriteLine(SumNum);
---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int size = 8;
+int[] array = new int[size];
+
+
+
+void FullArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+        Console.Write(array[i] + " ");
+    }
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " "); // через пробел
+    }
+    Console.WriteLine();
+}
+FullArray(array);
+PrintArray(array);
